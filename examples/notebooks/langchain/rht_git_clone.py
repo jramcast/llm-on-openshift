953ade00af75163a914c70d46958f4c992c01a03
Jaime Ramirez <jaime.ramirez@redhat.com>
2024-01-11 12:39:26 +0000

    add more courses to vector store

--- examples/notebooks/langchain/rht_git_clone.py
+++ examples/notebooks/langchain/rht_git_clone.py
@@ -3,15 +3,14 @@ from pathlib import Path
 from git import Repo
 
 
-
-def clone_course(sku):
+def clone_course(sku, branch="main"):
     username = os.getenv("GIT_USER")
     password = os.getenv("GIT_PASS")
-    
+
     Repo.clone_from(
         f"https://{username}:{password}@github.com/RedHatTraining/{sku}",
         to_path=Path.home() / f"courses/{sku}",
-        branch="main",
+        branch=branch,
         depth=1
     )
 
